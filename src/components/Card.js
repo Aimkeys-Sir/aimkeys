904f08381c762997e00f08af56198ba29a48a75d
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-12-04 18:25:21 +0300

    add responsivity

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,12 @@
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Card({ goToService, text, icon, projects, index, achieve, comment, technologies, works, expanded, setExpanded }) {
 
+    const [isPhone, setIsPhone] = useState(false)
+
+    useEffect((() => setIsPhone(window.innerWidth < 480)), [])
     function handleExpand() {
         if (expanded === text) {
             setExpanded(null)
@@ -15,25 +18,35 @@ export default function Card({ goToService, text, icon, projects, index, achieve
 
     function PictureCard({ item }) {
         return (
-            <a className="pic-link" key={index} href={item.go} style={{ position: "relative" }}>
-                <img src={item.image} alt={item.name} className="port-pic" />
-                <div className="invisible"></div>
-                {<div className="cover-div" >{item.name}{" "} <FontAwesomeIcon icon={faArrowUpRightFromSquare}/></div>}
-            </a>
+            <div className="pic-link">
+                <a target='_blank'  key={index} href={item.go}>
+                    <img src={item.image} alt={item.name} className="port-pic" />
+                    <div className="invisible"></div>
+                    {<div className="cover-div" >{item.name}{" "} <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></div>}
+                </a>
+            </div>
+
         )
     }
 
     return <div className="expander">
         {index > 1 ? <div className={`${text !== expanded ? "works" : "works-active"}`}>
-            <div className="port-pictures">
+            <div className="port-pictures desktop-p">
                 {works.map((item, index) => {
-                    return <PictureCard item={item} key={index}/>
+                    return <PictureCard item={item} key={index} />
                 })}
             </div>
         </div> : null}
         <div>
             <div className="card">
-                <FontAwesomeIcon onClick={handleExpand} className={`card-icon ${smaller ? "smaller-icon" : ""}`} icon={icon} />
+                <FontAwesomeIcon onClick={handleExpand} className={`card-icon ${smaller ? "smaller-icon" : ""} ${expanded === text ? "bigger-icon" : ""}`} icon={icon} />
+                {isPhone ? <div className={`${text !== expanded ? "works" : "works works-active"}`}>
+                    <div className="port-pictures phone-p">
+                        {works.map((item, index) => {
+                            return <PictureCard item={item} key={index} />
+                        })}
+                    </div>
+                </div> : null}
                 <h2>{text}</h2>
                 <p className="projects-span">{projects} projects</p>
                 <div className="technologies"> {technologies.map((tech, index) => {
@@ -53,12 +66,13 @@ export default function Card({ goToService, text, icon, projects, index, achieve
                 <p>{comment}</p>
             </div>
         </div>
-        {index < 2 ? <div className={`${text !== expanded ? "works" : "works works-active"}`}>
-            <div className="port-pictures">
+        {index < 2 && !isPhone ? <div className={`${text !== expanded ? "works" : "works works-active"}`}>
+            <div className="port-pictures desktop-p">
                 {works.map((item, index) => {
-                    return <PictureCard item={item} key={index}/>
+                    return <PictureCard item={item} key={index} />
                 })}
             </div>
         </div> : null}
+        {isPhone ? <div className="phone-line"></div> : null}
     </div>
 }
