da5d70c34937912305b9a4bc144a3673b378379c
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-12-04 19:37:29 +0300

    update images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,17 @@
 import "./home.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faDribbble, faGithub, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons"
+import { faGithub, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons"
 import { faAngleDown, faCircle, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons"
 import { useEffect, useState } from "react"
 
 export default function Home({ handleScrollTo }) {
     const [isPhone, setIsPhone] = useState(false)
-    const [showContacts, setShowContacts] = useState(false)
 
-    const [activeItem,setActiveItem] = useState("")
+    const [activeItem, setActiveItem] = useState("")
     const [textCopied, setTextCopied] = useState("")
 
     window.addEventListener('resize', () => {
-      setIsPhone(window.innerWidth < 480)
+        setIsPhone(window.innerWidth < 480)
     })
 
     useEffect((() => {
@@ -24,17 +23,23 @@ export default function Home({ handleScrollTo }) {
     }))
 
     function handleContactOnclick() {
-       setActiveItem('contacts')
+        if(activeItem === 'contacts') {
+            setActiveItem("")
+        }else{
+            setActiveItem('contacts')
+        }
+        
     }
-     function copyToClipBoard(text, value){
+    function copyToClipBoard(text, value) {
         navigator.clipboard.writeText(value)
-        setTextCopied(text)
-     }
-     if(textCopied){
+            .then(() => setTextCopied(text))
+
+    }
+    if (textCopied) {
         setTimeout(() => {
             setTextCopied("")
         }, 2000);
-     }
+    }
 
 
     return (
@@ -46,19 +51,19 @@ export default function Home({ handleScrollTo }) {
                 </div>
                 <div className="links">
                     <h2 onClick={handleScrollTo}>Portfolio</h2>
-                    <h2 className={`${activeItem === "blogs"?"c-active":""}`} onClick={()=>setActiveItem('blogs')}>Blogs</h2>
+                   <a style={{textDecoration: 'none',color:"white"}} href="https://medium.com/@kelvostein/javascript-a-project-a-day-bf02d5d0ffc0" target='_blank'>  <h2 className={`${activeItem === "blogs" ? "c-active" : ""}`} onClick={() => setActiveItem('blogs')}>Blogs</h2></a>
                     <div className="contacts-div">
-                        <h2 onBlur={()=>setActiveItem("")} onClick={handleContactOnclick} className={`contact-line ${activeItem === "contacts"?"c-active":""}`}>Contact</h2>
+                       <h2 onBlur={() => setActiveItem("")} onClick={handleContactOnclick} className={`contact-line ${activeItem === "contacts" ? "c-active" : ""}`}>Contact</h2>
                         {activeItem === "contacts" ? <div className="phone-icons">
                             <div className="active-contacts-line"></div>
                             <div className="all-icons">
                                 {/* <div><FontAwesomeIcon className="b-icons twitter" icon={faTwitter} /></div> */}
-                                <div className="contact-items"><div>aimkeys.mwaura@gmail.com</div>  <FontAwesomeIcon  onClick={()=>copyToClipBoard("email","aimkeys.mwaura@gmail.com")} className="b-icons email" icon={faEnvelope} /></div>
-                                <div className="contact-items"><div>+254 795 217 556</div><FontAwesomeIcon onClick={()=>copyToClipBoard("phone no.","+254795217556")} className="b-icons phonee" icon={faPhone} /></div>
+                                <div className="contact-items"><div>aimkeys.mwaura@gmail.com</div>  <FontAwesomeIcon onClick={() => copyToClipBoard("email", "aimkeys.mwaura@gmail.com")} className="b-icons email" icon={faEnvelope} /></div>
+                                <div className="contact-items"><div>+254 795 217 556</div><FontAwesomeIcon onClick={() => copyToClipBoard("phone no.", "+254795217556")} className="b-icons phonee" icon={faPhone} /></div>
                                 {/* <div></div> */}
                             </div>
                         </div> : null}
-                        {textCopied?<p className="text-copied">{textCopied} copied!</p>: null}
+                        {textCopied ? <p className="text-copied">{textCopied} copied!</p> : null}
                     </div>
                 </div>
 
@@ -75,14 +80,14 @@ export default function Home({ handleScrollTo }) {
                     <div className="line"></div>
 
                     <div className="contacts up">
-                        <a target='_blank' href="https://twitter.com/aimkeys_m"><h4>Twitter</h4> <FontAwesomeIcon className="b-icon twitter" icon={faTwitter}/></a>
+                        <a target='_blank' href="https://twitter.com/aimkeys_m"><h4>Twitter</h4> <FontAwesomeIcon className="b-icon twitter" icon={faTwitter} /></a>
                         <h4>|</h4>
                         <a target='_blank' href="https://www.linkedin.com/in/kelvin-aimkeys-mwaura-654447244/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3B59jwljWAScC7grxd1d7wEg%3D%3D"><h4>LinkedIn</h4><FontAwesomeIcon className="b-icon linkedin" icon={faLinkedin} /></a>
                     </div>
                     <div className="contacts down">
                         <a target='_blank' href="https://github.com/Aimkeys-Sir"><h4>Github</h4> <FontAwesomeIcon className="b-icon github" icon={faGithub} /></a>
                         <h4>|</h4>
-                        <a target='_blank' href="https://www.codewars.com/users/Aimkeys-Sir"><h4>CodeWars</h4><img className="codewars" src="/icons/codewars.svg" alt="codewars"/></a>
+                        <a target='_blank' href="https://www.codewars.com/users/Aimkeys-Sir"><h4>CodeWars</h4><img className="codewars" src="/icons/codewars.svg" alt="codewars" /></a>
                     </div>
 
                     <div onClick={() => window.open("mailto:aimkeys.mwaura@gmail.com?subject=Talk to Aimkeys&body=/*Hello there. You can always send me a email, I reply quite fast, or use my socials, thank you!*/")} className="contact-button">
@@ -94,11 +99,11 @@ export default function Home({ handleScrollTo }) {
                     {/* <img id="picture" src="/mk2.png" alt="mk" /> */}
 
                 </div>
-                {isPhone? null:
-                <div className="blocks-for-life">
-                    <div className="blocks">Blocks</div>
-                    <div className="for-life">For Life</div>
-                </div>}
+                {isPhone ? null :
+                    <div className="blocks-for-life">
+                        <div className="blocks">Blocks</div>
+                        <div className="for-life">For Life</div>
+                    </div>}
                 <div className="introduction">
                     <h1>I am</h1>
                     <h3 className="intro">A Full Stack Web Developer</h3>
@@ -112,7 +117,7 @@ export default function Home({ handleScrollTo }) {
                 </div>
 
             </div>
-            {isPhone?null:<div className="go-down">
+            {isPhone ? null : <div className="go-down">
                 <FontAwesomeIcon className="go-down-icon" onClick={handleScrollTo} icon={faAngleDown} />
             </div>}
         </div>
--- src/components/Services.js
+++ src/components/Services.js
@@ -10,16 +10,16 @@ export default function Services({ goToService }) {
 
     const fields = [
         { text: "Web Development", projects: 12, icon: faCode, comment: "Years of experience", achieve: 3, technologies: [{ icon: faReact }, { icon: faJs }, { icon: faCss3 }, { source: 'icons/ruby.svg' }],works:  [{image:"/images/murife.png",name:"Murife Comparison App",go:"https://github.com/Aimkeys-Sir/online-comm-frontend"},{image:"/images/weather.png",name:"Awesome Weather", go:"https://github.com/Aimkeys-Sir/my-weather"}]},
-        { text: "UI/UX Design", projects: 3, icon: faPenNib, comment: "Design items", achieve: 25, technologies: [{ icon: faFigma }, { source: '/icons/illustrator.svg' }], works:  [{image:"/images/murife.png",name:"Murife Comparison App",go:"https://github.com/Aimkeys-Sir/online-comm-frontend"},{image:"/images/weather.png",name:"Awesome Weather", go:"https://github.com/Aimkeys-Sir/my-weather"}]},
-        { text: "Game Development", projects: 15, icon: faGamepad, comment: "Projects completed", achieve: 4, technologies: [{ icon: faUnity }, { source: '/icons/csharp.png' }], works:  [{image:"/images/watch.png", name:"models collection",go:"https://github.com/Aimkeys-Sir/aimkeys-collections/tree/main/models/Solidworks"},{image:"/images/camaro.jpg",name:"Magazine renders",go:"https://github.com/Aimkeys-Sir/aimkeys-collections/tree/main/magazine-renders"}] },
-        { text: "3D Modelling", projects: 120, icon: faCubes, comment: "Customers served", achieve: 30, technologies: [{ source: "/icons/blender.svg" }, { source: '/icons/solidworks.png' }], works:  [{image:"/images/watch.png", name:"models collection",go:"https://github.com/Aimkeys-Sir/aimkeys-collections/tree/main/models/Solidworks"},{image:"/images/camaro.jpg",name:"Magazine renders",go:"https://github.com/Aimkeys-Sir/aimkeys-collections/tree/main/magazine-renders"}] }
+        { text: "UI/UX Design", projects: 3, icon: faPenNib, comment: "Design items", achieve: 25, technologies: [{ icon: faFigma }, { source: '/icons/illustrator.svg' }], works:  [{image:"/images/gameplace.png",name:"Murife Comparison App",go:"https://github.com/Aimkeys-Sir/online-comm-frontend"},{image:"/images/weather.png",name:"Awesome Weather", go:"https://github.com/Aimkeys-Sir/my-weather"}]},
+        { text: "Game Development", projects: 15, icon: faGamepad, comment: "Projects completed", achieve: 4, technologies: [{ icon: faUnity }, { source: '/icons/csharp.png' }], works:  [{image:"/images/camaro.jpg", name:"models collection",go:"https://github.com/Aimkeys-Sir/aimkeys-collections/tree/main/models/Solidworks"},{image:"/images/sword.jpg",name:"Magazine renders",go:"https://github.com/Aimkeys-Sir/aimkeys-collections/tree/main/magazine-renders"}] },
+        { text: "3D Modelling", projects: 120, icon: faCubes, comment: "Customers served", achieve: 30, technologies: [{ source: "/icons/blender.svg" }, { source: '/icons/solidworks.png' }], works:  [{image:"/images/watch.png", name:"models collection",go:"https://github.com/Aimkeys-Sir/aimkeys-collections/tree/main/models/Solidworks"},{image:"/images/captain.jpg",name:"Magazine renders",go:"https://github.com/Aimkeys-Sir/aimkeys-collections/tree/main/magazine-renders"}] }
     ]
 
     return (
         <div onMouseLeave={()=>setExpanded(null)} className="services page">
             <div className="tops">
-                <h2>SOME OF MY WORK</h2>
-                <p> </p>
+                <h2 style={{color:"white"}}>AIMKEYS' </h2>
+                <h2>WORK</h2>
             </div>
             <div  className="cards">
                 {fields.map((field, index) => {
